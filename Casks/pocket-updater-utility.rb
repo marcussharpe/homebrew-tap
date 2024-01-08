92b4ef4b9591dc685e7d20df57f4c1e3a384cd5f
Marcus Sharpe <marcus@sharpe.xyz>
2024-01-08 20:57:47 +0000

    pocket-updater-utility 2.43.1

--- Casks/pocket-updater-utility.rb
+++ Casks/pocket-updater-utility.rb
@@ -1,6 +1,6 @@
 cask "pocket-updater-utility" do
-    version "2.43.0"
-    sha256 "b7331167929d5537bd93c1f7780f14fcbb829de4f41e2acb4d864645d04650e0"
+    version "2.43.1"
+    sha256 "4e9ae58c37e0ed62d449db285708ae64ddf32f16f23781217d7e56a01550167d"
     url "https://github.com/mattpannella/pocket-updater-utility/releases/download/#{version}/pocket_updater_mac.zip"
     name "Pocket Updater Utility"
     desc "Utility for updating the openFPGA cores, and firmware, on your Analogue Pocket"
