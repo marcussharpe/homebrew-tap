2ff33c718dfd67fc51a0d01b3599ebe6f3fd0a31
Marcus Sharpe <marcus@sharpe.xyz>
2024-11-03 14:25:39 +0000

    pupdate 3.19.1

--- Casks/pupdate.rb
+++ Casks/pupdate.rb
@@ -1,6 +1,6 @@
 cask "pupdate" do
-    version "3.19.0"
-    sha256 "6c514c3af2c74b98dcb34e43c7cc7249ed6e2701611c03eb07cc81018b2c4e3e"
+    version "3.19.1"
+    sha256 "b6da57ac897963b95c7fc415e353ce03755ed59cdaab4041bce506124cb6ef79"
     url "https://github.com/mattpannella/pupdate/releases/download/#{version}/pupdate_mac.zip"
     name "Pupdate"
     desc "Utility for updating the openFPGA cores, firmware, and a bunch of other stuff on your Analogue Pocket."
